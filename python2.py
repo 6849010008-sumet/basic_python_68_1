929da496bc9b12c175b22535df406b688554e0ae
6849010008-sumet <6849010008@phuketvc.ac.th>
2026-01-31 13:56:57 +0700

    make a python2.py

---
+++ python2.py
@@ -0,0 +1,57 @@
+"""
+#
+# Part: Python Operators
+# Arithmetic Operators
+#
+"""
+x = 4 + 5 # Addition
+y = 10 - 2 # Subtraction
+z = 3 * 7 # Multiplication
+a = 8 / 2 # Division
+b = 10 % 3 # Modulus
+c = 2 ** 3 #Exponetiation
+
+"""
+#
+# Part: Python Operators
+# Assignment Operators
+#
+"""
+
+x = 5
+x += 3
+x = x + 3
+
+"""
+#
+# Part: Python Operators
+# Comparion Operators
+#
+"""
+
+a = 5
+b = 10
+print(a == b)
+print(a != b)
+print(a > b)
+print(a < b)
+print(a >= b)
+print(a <= b)
+
+"""
+#
+# Part: Python Operators
+# Logical Operators
+#
+"""
+x = True
+y = False
+print(x and y)
+print(x or y)
+print(not x)
+print(x & y)
+print(x | y)
+print(~x)
+a = 5
+b = 10
+print((a < 10) and (b > 5))
